d44d41e0a7f18c1ba9549a56eab3773bb68631aa
Joshua Krall <joshuakrall@pobox.com>
2011-08-17 02:08:35 -0500

    Stub out OrdersController for Samurai integration

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,14 @@
+class OrdersController < ApplicationController
+
+  # Create/Update a payment method via Samurai Transparent Redirect
+  def payment_method
+  end
+
+  # Create a new transaction with a payment_method
+  def transaction
+  end
+
+  def show
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Blog::Application.routes.draw do
+  get "orders#payment_method"
+  get "orders#transaction"
+  get "orders#show"
+
   resources :articles
   root :to => "articles#index"
 end
