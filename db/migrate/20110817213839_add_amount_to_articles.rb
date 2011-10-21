420efbd28ca41e4387b0c4ba4968396a1d0f32b1
Jared Hardy <jared@jaredhardy.com>
2011-10-21 10:41:11 -0700

    removing duplicate migrations

--- db/migrate/20110817213839_add_amount_to_articles.rb
+++
@@ -1,9 +0,0 @@
-class AddAmountToArticles < ActiveRecord::Migration
-  def self.up
-    add_column :articles, :amount, :float
-  end
-
-  def self.down
-    remove_column :articles, :amount
-  end
-end
